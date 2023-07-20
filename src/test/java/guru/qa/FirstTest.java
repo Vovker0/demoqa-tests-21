245773afbf2cbea56cfef465c3b8a4c047ac63e7
Vladimir <none@none>
2023-07-19 21:05:33 -0400

    Added 3 tests exapmples

---
+++ src/test/java/guru/qa/FirstTest.java
@@ -0,0 +1,47 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+
+public class FirstTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("beforeAll()");
+        Configuration.browser = "Chrome";
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("afterAll()");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+//        open("http://google.com");
+        System.out.println("beforeEach()");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("afterEach()");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("            First test");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("            Second test");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("            Third test");
+        Assertions.assertTrue(3 > 2);
+    }
+}
